301690ae020e26add968111a84d62b20da692aea
kiranjadhavcreditsystems <77472898+kiranjadhavcreditsystems@users.noreply.github.com>
2021-05-16 11:16:24 +0530

    CSI HRM initial commit

---
+++ EmployeeDatabase/src/test/java/com/csi/EmployeeDatabase/EmployeeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csi.EmployeeDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
