db655ba0aa73e89d994a8cbe69632f25f5a4b9d0
admin <admin@192.168.1.208>
2021-05-16 13:21:14 +0530

    Added Employee Pojo class

--- EmployeeDatabase/src/main/java/com/csi/EmployeeDatabaseApplication.java
+++ EmployeeDatabase/src/main/java/com/csi/EmployeeDatabaseApplication.java
@@ -1,4 +1,4 @@
-package com.csi.EmployeeDatabase;
+package com.csi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ EmployeeDatabase/src/main/java/com/csi/model/Employee.java
@@ -0,0 +1,5 @@
+package com.csi.model;
+
+public class Employee {
+
+}
