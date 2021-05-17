f5b1aaaa3c4e9f61153ac0100d3085fc860ec56d
Abhay <abhaywani130@gmail.com>
2021-05-17 10:25:54 +0530

    Refactor employee pojo class

--- EmployeeDatabase/src/main/java/com/csi/model/Employee.java
+++ EmployeeDatabase/src/main/java/com/csi/model/Employee.java
@@ -1,67 +1,35 @@
 package com.csi.model;
 
-	import java.util.Date;
+import java.util.Date;
 
-	import lombok.Builder;
-	import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-	@Builder
-	@Data
-	public class Employee {
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
 
-		private int empId;
-		private String empFirstName;
-		private String empLastName;
-		private String empEmailId;
-		private String empPassword;
-		private String empDesignation;
-		private String empCurrentAddress;
-		private String empPermenentAddress;
-		private String empMaritalStatus;
-		private String empBloodGroup;
-		private String empGender;
-		private String empQualification;
-		private long empContactNumber;
-		private long empUID;
-		private String empPANCardNumber;
-		private double empSalary;
-		private Date empJoiningDate;
-		private boolean empStatus;
-		
-		
-		
-		public Employee()    //To use builder design pattern requried constructor
-		{
-			
-		}
-		
-		
-		public Employee(int empId, String empFirstName, String empLastName, String empEmailId, String empPassword,
-				String empDesignation, String empCurrentAddress, String empPermenentAddress, String empMaritalStatus,
-				String empBloodGroup, String empGender, String empQualification, long empContactNumber, long empUID,
-				String empPANCardNumber, double empSalary, Date empJoiningDate, boolean empStatus) {
-			super();
-			this.empId = empId;
-			this.empFirstName = empFirstName;
-			this.empLastName = empLastName;
-			this.empEmailId = empEmailId;
-			this.empPassword = empPassword;
-			this.empDesignation = empDesignation;
-			this.empCurrentAddress = empCurrentAddress;
-			this.empPermenentAddress = empPermenentAddress;
-			this.empMaritalStatus = empMaritalStatus;
-			this.empBloodGroup = empBloodGroup;
-			this.empGender = empGender;
-			this.empQualification = empQualification;
-			this.empContactNumber = empContactNumber;
-			this.empUID = empUID;
-			this.empPANCardNumber = empPANCardNumber;
-			this.empSalary = empSalary;
-			this.empJoiningDate = empJoiningDate;
-			this.empStatus = empStatus;
-		}
+	private int empId;
+	private String empFirstName;
+	private String empLastName;
+	private String empEmailId;
+	private String empPassword;
+	private String empDesignation;
+	private String empCurrentAddress;
+	private String empPermenentAddress;
+	private String empMaritalStatus;
+	private String empBloodGroup;
+	private String empGender;
+	private String empQualification;
+	private long empContactNumber;
+	private long empUID;
+	private String empPANCardNumber;
+	private double empSalary;
+	private Date empJoiningDate;
+	private boolean empStatus;
 
-		
-	}
-
-
+}
